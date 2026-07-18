88be89a5d8041f9878af6a2caf1685e1ca6706d8
Allwrighty3 <tylerwright200012@gmail.com>
2026-07-17 22:42:50 -0600

    Test local action contingency experiment

---
+++ test/procession/simulation/action_contingency_experiment_test.exs
@@ -0,0 +1,34 @@
+defmodule Procession.Simulation.ActionContingencyExperimentTest do
+  use ExUnit.Case, async: true
+
+  alias Procession.Simulation.ActionContingencyExperiment, as: Experiment
+
+  test "comparison includes reliable and noisy motor regimes" do
+    results = Experiment.compare(ticks: 40, seeds: Enum.to_list(1..4))
+
+    assert Map.has_key?(results, {:reliable, :reactive})
+    assert Map.has_key?(results, {:reliable, :contingency_adaptive})
+    assert Map.has_key?(results, {:noisy, :outcome_adaptive})
+  end
+
+  test "contingency learner confirms only temporally overlapping action effects" do
+    state = Experiment.run(
+      variant: :contingency_adaptive,
+      motor_mode: :reliable,
+      ticks: 80,
+      seed: 7,
+      effect_delay: 2
+    )
+
+    assert state.confirmed_contingencies > 0
+    assert state.false_credit == 0
+  end
+
+  test "world-facing state does not expose a causal history API" do
+    state = Experiment.run(ticks: 1)
+
+    refute Map.has_key?(state, :world_provenance)
+    refute Map.has_key?(state, :cause_graph)
+    assert is_map(state.traces)
+  end
+end
