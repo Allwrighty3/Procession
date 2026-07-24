ae49d3b7918ad86898d09b8e5524b35b8fd18481
Allwrighty3 <tylerwright200012@gmail.com>
2026-07-23 23:15:50 -0700

    Use bounded primitive substrate validation run

--- lib/mix/tasks/procession.metrics.sibling_signal_followup.ex
+++ lib/mix/tasks/procession.metrics.sibling_signal_followup.ex
@@ -5,7 +5,12 @@ defmodule Mix.Tasks.Procession.Metrics.SiblingSignalFollowup do
 
   @impl true
   def run(_args) do
-    Procession.Simulation.PrimitiveDevelopmentExperiment.run(population: 4)
+    Procession.Simulation.PrimitiveDevelopmentExperiment.run(
+      population: 4,
+      baby_ticks: 500,
+      participation_ticks: 500,
+      withdrawal_ticks: 1_000
+    )
     |> Procession.Simulation.PrimitiveDevelopmentExperiment.report()
     |> Mix.shell().info()
   end
