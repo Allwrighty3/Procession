c85247c04f2c833f5a3b58a7ac3b0ed102b1e1f3
Allwrighty3 <tylerwright200012@gmail.com>
2026-07-23 23:18:04 -0700

    Bound primitive mechanism probe

--- lib/mix/tasks/procession.metrics.sibling_signal_followup.ex
+++ lib/mix/tasks/procession.metrics.sibling_signal_followup.ex
@@ -6,10 +6,10 @@ defmodule Mix.Tasks.Procession.Metrics.SiblingSignalFollowup do
   @impl true
   def run(_args) do
     Procession.Simulation.PrimitiveDevelopmentExperiment.run(
-      population: 4,
-      baby_ticks: 500,
-      participation_ticks: 500,
-      withdrawal_ticks: 1_000
+      population: 2,
+      baby_ticks: 100,
+      participation_ticks: 100,
+      withdrawal_ticks: 200
     )
     |> Procession.Simulation.PrimitiveDevelopmentExperiment.report()
     |> Mix.shell().info()
