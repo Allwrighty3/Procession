87fd703d4ba9055a0c3cce7b5db11254c2c7dc1c
Allwrighty3 <tylerwright200012@gmail.com>
2026-07-25 15:37:40 -0700

    Support restoring and capturing developmental minds

--- lib/procession/entity/entity_supervisor.ex
+++ lib/procession/entity/entity_supervisor.ex
@@ -11,16 +11,15 @@ defmodule Procession.EntitySupervisor do
   are opt-in and registered under a private composite key tied to the entity ID.
   """
 
-  def start_link(_opts) do
-    DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
-  end
+  def start_link(_opts), do: DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
 
   @doc """
   Starts a new entity process registered by its ID.
 
-  Pass `sensorimotor: keyword_options` in attrs to attach a persistent developmental
-  sensorimotor owner. The option is consumed by the supervisor and is not exposed as
-  entity metadata.
+  Pass `sensorimotor: keyword_options` in attrs to attach a developmental owner.
+  The options may include `snapshot:` for a bounded dormant snapshot or `loop:` for
+  an exact short-lived rollback checkpoint. These options are consumed by the
+  supervisor and are not exposed as entity metadata.
   """
   def start_entity(id, attrs) do
     {sensorimotor_opts, entity_attrs} = Map.pop(attrs, :sensorimotor)
@@ -28,34 +27,20 @@ defmodule Procession.EntitySupervisor do
     case start_entity_process(id, entity_attrs) do
       {:ok, entity_pid} ->
         case maybe_start_sensorimotor(id, entity_pid, sensorimotor_opts) do
-          :ok ->
-            {:ok, entity_pid}
-
+          :ok -> {:ok, entity_pid}
           {:error, reason} ->
             DynamicSupervisor.terminate_child(__MODULE__, entity_pid)
             {:error, reason}
         end
 
-      error ->
-        error
+      error -> error
     end
   end
 
-  def start_player(id, attrs \\ %{}) do
-    start_entity(id, Map.put(attrs, :type, :player))
-  end
-
-  def start_npc(id, attrs \\ %{}) do
-    start_entity(id, Map.put(attrs, :type, :npc))
-  end
-
-  def start_location(id, attrs \\ %{}) do
-    start_entity(id, Map.put(attrs, :type, :location))
-  end
-
-  def start_faction(id, attrs \\ %{}) do
-    start_entity(id, Map.put(attrs, :type, :faction))
-  end
+  def start_player(id, attrs \\ %{}), do: start_entity(id, Map.put(attrs, :type, :player))
+  def start_npc(id, attrs \\ %{}), do: start_entity(id, Map.put(attrs, :type, :npc))
+  def start_location(id, attrs \\ %{}), do: start_entity(id, Map.put(attrs, :type, :location))
+  def start_faction(id, attrs \\ %{}), do: start_entity(id, Map.put(attrs, :type, :faction))
 
   def enable_sensorimotor(id, opts \\ []) when is_list(opts) do
     case lookup_entity(id) do
@@ -65,59 +50,46 @@ defmodule Procession.EntitySupervisor do
           [] -> start_sensorimotor(id, entity_pid, opts)
         end
 
-      {:error, :not_found} ->
-        {:error, :entity_not_found}
+      {:error, :not_found} -> {:error, :entity_not_found}
     end
   end
 
-  def sensorimotor_enabled?(id) do
-    match?([{_pid, _value}], Registry.lookup(Procession.EntityRegistry, {:sensorimotor, id}))
-  end
+  def sensorimotor_enabled?(id),
+    do: match?([{_pid, _value}], Registry.lookup(Procession.EntityRegistry, {:sensorimotor, id}))
 
   def sensorimotor_cycle(id, features, tick, feedback_fun, opts \\ []) do
-    with :ok <- require_sensorimotor(id) do
-      LiveSensorimotor.cycle(id, features, tick, feedback_fun, opts)
-    end
+    with :ok <- require_sensorimotor(id), do: LiveSensorimotor.cycle(id, features, tick, feedback_fun, opts)
   end
 
   def sensorimotor_observe(id, features, opts \\ []) do
-    with :ok <- require_sensorimotor(id) do
-      LiveSensorimotor.observe(id, features, opts)
-    end
+    with :ok <- require_sensorimotor(id), do: LiveSensorimotor.observe(id, features, opts)
   end
 
   def sensorimotor_emit(id, features, tick, opts \\ []) do
-    with :ok <- require_sensorimotor(id) do
-      LiveSensorimotor.emit(id, features, tick, opts)
-    end
+    with :ok <- require_sensorimotor(id), do: LiveSensorimotor.emit(id, features, tick, opts)
   end
 
   def sensorimotor_resolve(id, position, features, coherence, opts \\ []) do
-    with :ok <- require_sensorimotor(id) do
-      LiveSensorimotor.resolve(id, position, features, coherence, opts)
-    end
+    with :ok <- require_sensorimotor(id), do: LiveSensorimotor.resolve(id, position, features, coherence, opts)
   end
 
   def sensorimotor_feedback(id, features, coherence, opts \\ []) do
-    with :ok <- require_sensorimotor(id) do
-      LiveSensorimotor.feedback(id, features, coherence, opts)
-    end
+    with :ok <- require_sensorimotor(id), do: LiveSensorimotor.feedback(id, features, coherence, opts)
+  end
+
+  def sensorimotor_snapshot(id, opts \\ []) do
+    with :ok <- require_sensorimotor(id), do: LiveSensorimotor.snapshot(id, opts)
+  end
+
+  def sensorimotor_checkpoint(id) do
+    with :ok <- require_sensorimotor(id), do: LiveSensorimotor.checkpoint(id)
   end
 
   def sensorimotor_trace(id) do
-    if sensorimotor_enabled?(id) do
-      {:ok, LiveSensorimotor.trace(id)}
-    else
-      {:error, :sensorimotor_not_enabled}
-    end
+    if sensorimotor_enabled?(id), do: {:ok, LiveSensorimotor.trace(id)}, else: {:error, :sensorimotor_not_enabled}
   end
 
-  def exists?(id) do
-    case Registry.lookup(Procession.EntityRegistry, id) do
-      [{_pid, _value}] -> true
-      [] -> false
-    end
-  end
+  def exists?(id), do: match?([{_pid, _value}], Registry.lookup(Procession.EntityRegistry, id))
 
   def stop_entity(id) do
     stop_sensorimotor(id)
@@ -143,7 +115,6 @@ defmodule Procession.EntitySupervisor do
 
   def create_npc(attrs \\ %{}) do
     id = Procession.Id.npc()
-
     case start_npc(id, attrs) do
       {:ok, pid} -> {:ok, id, pid}
       error -> error
@@ -152,7 +123,6 @@ defmodule Procession.EntitySupervisor do
 
   def create_location(attrs \\ %{}) do
     id = Procession.Id.location()
-
     case start_location(id, attrs) do
       {:ok, pid} -> {:ok, id, pid}
       error -> error
@@ -161,7 +131,6 @@ defmodule Procession.EntitySupervisor do
 
   def create_faction(attrs \\ %{}) do
     id = Procession.Id.faction()
-
     case start_faction(id, attrs) do
       {:ok, pid} -> {:ok, id, pid}
       error -> error
@@ -169,9 +138,7 @@ defmodule Procession.EntitySupervisor do
   end
 
   @impl true
-  def init(:ok) do
-    DynamicSupervisor.init(strategy: :one_for_one)
-  end
+  def init(:ok), do: DynamicSupervisor.init(strategy: :one_for_one)
 
   defp start_entity_process(id, attrs) do
     child_spec = {Entity, id: id, state: Map.put(attrs, :id, id)}
@@ -188,10 +155,13 @@ defmodule Procession.EntitySupervisor do
   end
 
   defp start_sensorimotor(id, entity_pid, opts) do
-    child_spec =
-      {LiveSensorimotor, [entity_id: id, owner_pid: entity_pid, loop_opts: opts]}
+    child_opts =
+      [entity_id: id, owner_pid: entity_pid]
+      |> maybe_put(:snapshot, Keyword.get(opts, :snapshot))
+      |> maybe_put(:loop, Keyword.get(opts, :loop))
+      |> Keyword.put(:loop_opts, Keyword.drop(opts, [:snapshot, :loop]))
 
-    DynamicSupervisor.start_child(__MODULE__, child_spec)
+    DynamicSupervisor.start_child(__MODULE__, {LiveSensorimotor, child_opts})
   end
 
   defp stop_sensorimotor(id) do
@@ -204,4 +174,7 @@ defmodule Procession.EntitySupervisor do
   defp require_sensorimotor(id) do
     if sensorimotor_enabled?(id), do: :ok, else: {:error, :sensorimotor_not_enabled}
   end
+
+  defp maybe_put(opts, _key, nil), do: opts
+  defp maybe_put(opts, key, value), do: Keyword.put(opts, key, value)
 end
